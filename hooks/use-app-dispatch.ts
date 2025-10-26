256638461d1d38905172499fc3834c81e6d7fa2a
BrandonLara <20221091@uthh.edu.mx>
2025-10-26 16:27:01 -0600

    modificacion de hooks/use-app-dispatch.ts

--- hooks/use-app-dispatch.ts
+++ hooks/use-app-dispatch.ts
@@ -1,8 +1,13 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "@/store";
 
-// Para usar dispatch con thunks tipados
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+// Deriva AppDispatch del store real
+export type AppDispatch = typeof import("@/store").store.dispatch;
 
-// Para que useSelector ya tenga el tipo RootState
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// useDispatch: castea el resultado (evita TS2347)
+export const useAppDispatch = () => useDispatch() as AppDispatch;
+
+// useSelector: tipa el state a RootState sin TypedUseSelectorHook
+export const useAppSelector = useSelector as unknown as <
+  TSelected
+>(selector: (state: RootState) => TSelected, equalityFn?: (a: TSelected, b: TSelected) => boolean) => TSelected;
